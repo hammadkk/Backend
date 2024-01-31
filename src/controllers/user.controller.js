66b2913f5c18fae5f48f623eca9230aae7c2f6f6
Hammad Khan <85316531+hammadkk@users.noreply.github.com>
2024-02-01 00:22:40 +0500

    added regsiter fucntionality

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
+  console.log("password: ", password);
+});
+export { registerUser };
